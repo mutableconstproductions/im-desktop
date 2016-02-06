d9edfb2235c25eb5c7c070b6a92baa93f30b232e
juszczakn <juszczakn@gmail.com>
2016-02-06 12:10:52 -0600

    sending messages correctly

--- test.js
+++ test.js
@@ -46,15 +46,30 @@ myApp.factory('Message', function($http) {
 
 myApp.controller('MyCtrl', function($scope, Contact, Message) {
     let self = this;
+    // globally set uuidIdentity from main process
     self.identity = uuidIdentity;
-    self.contacts = null;
+    self.contacts = [];
+    self.messageThread = "";
+    self.currentContact = null;
 
     Contact.getContacts(self.identity).then(function(response) {
         self.contacts = response.data.data;
+        // TODO check to see if exists? 
+        self.contacts.map(function(contact) { contact.thread = []; });
+        // TODO dont do this
+        self.currentContact = self.contacts[0];
     });
 
     self.send = function() {
-        Message.sendMessage(self.identity, '555-5555', self.currentMessage);
-        self.currentMessage = null;
+        Message.sendMessage(self.identity, self.currentContact.mobile, self.currentMessage);
+        let displayedMessage = 'Me: ' + self.currentMessage + "\n";
+        self.currentContact.thread.push(displayedMessage);
+        self.messageThread += displayedMessage;
+        self.currentMessage = '';
+    }
+
+    self.switchContact = function(mobile) {
+        self.currentContact = self.contacts.filter(function(contact) { return contact.mobile === mobile; })[0];
+        self.messageThread = self.currentContact.thread.reduce(function(all, it) { return all + it; }, '');
     }
 });
