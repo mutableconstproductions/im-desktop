d58fd9470eaa662a2325bf3fe19e558719a4465e
juszczakn <juszczakn@gmail.com>
2016-02-16 22:24:07 -0600

    add register on startup, thread timestamps

--- test.js
+++ test.js
@@ -3,14 +3,34 @@
 let myApp = angular.module('myApp', []);
 
 myApp.constant('Constants', {
-    url: 'http://localhost:8080'
+    url: 'http://localhost:8080',
+    appName: 'Desktop'
 });
 
-myApp.factory('Contact', function($http) {
+myApp.factory('Register', function($http, Constants) {
+    // (val clientId: UUID, val appName: String)
+    function register(uuid) {
+        let packet = {
+            clientId: uuid,
+            appName: Constants.appName
+        };
+        return $http({
+            method: 'POST',
+            url: Constants.url + '/register',
+            data: packet
+        });
+    }
+
+    return {
+        register: register
+    }
+});
+
+myApp.factory('Contact', function($http, Constants) {
     function getContacts(uuid) {
         return $http({
             method: 'GET',
-            url: 'http://localhost:8080/getContacts?' + 'clientId=' + uuid
+            url: Constants.url + '/getContacts' + '?clientId=' + uuid
         }).success(function(response) {
             return response;
         }).error(function() {
@@ -62,7 +82,7 @@ myApp.factory('Message', function($http, Constants) {
 });
 
 
-myApp.controller('MyCtrl', function($scope, Constants, Contact, Message) {
+myApp.controller('MyCtrl', function($scope, Constants, Register, Contact, Message) {
     let self = this;
     self.identity = uuidIdentity;
     self.contacts = [];
@@ -70,10 +90,17 @@ myApp.controller('MyCtrl', function($scope, Constants, Contact, Message) {
     self.messageThread = "";
     self.currentContact = null;
 
+    function formatTimestamp(time) {
+        return '(' + (new Date(time)).toLocaleTimeString() + ') ';
+    }
+
+    function formatTimestampNow() {
+        return formatTimestamp((new Date()).getTime());
+    }
 
     self.send = function() {
         Message.sendMessage(self.identity, self.currentContact.mobile, self.currentMessage);
-        let displayedMessage = 'Me: ' + self.currentMessage + "\n";
+        let displayedMessage = formatTimestampNow() + 'Me: ' + self.currentMessage + "\n";
         self.currentContact.thread.push(displayedMessage);
         self.messageThread += displayedMessage;
         self.currentMessage = '';
@@ -87,8 +114,7 @@ myApp.controller('MyCtrl', function($scope, Constants, Contact, Message) {
     self.processNewMessages = function(messages) {
         for (let msg of messages) {
             let contact = self.contactsByMobile[msg.mobile];
-            let timestamp = '(' + (new Date(msg.dateSent)).toLocaleTimeString() + ') ';
-            let newMsg = timestamp + contact.name + ': ' + msg.text + '\n';
+            let newMsg = formatTimestamp(msg.dateSent) + contact.name + ': ' + msg.text + '\n';
             contact.thread.push(newMsg);
             if (self.currentContact.mobile === contact.mobile) {
                 self.messageThread += newMsg;
@@ -96,22 +122,29 @@ myApp.controller('MyCtrl', function($scope, Constants, Contact, Message) {
         }
     }
 
-    Contact.getContacts(self.identity).then(function(response) {
-        self.contacts = response.data.data;
-        // TODO check to see if exists?
-        self.contacts.map(function(contact) { contact.thread = []; });
-        for (let contact of self.contacts) {
-            self.contactsByMobile[contact.mobile] = contact;
-        }
-        // TODO dont do this
-        self.currentContact = self.contacts[0];
-    }).then(function() {
-        window.setInterval(function() {
-            Message.getNewMessages(self.identity).then(function(response) {
-                if (response !== null && response.data.length !== 0) {
-                    self.processNewMessages(response.data);
-                }
-            });
-        }, 2000);
+    /*
+     * Identity ourselves by registering and then
+     * proceed to retrieve contacts
+     */
+    Register.register(self.identity).then(function() {
+        Contact.getContacts(self.identity).then(function(response) {
+            self.contacts = response.data.data;
+            // TODO check to see if exists?
+            self.contacts.map(function(contact) { contact.thread = []; });
+            for (let contact of self.contacts) {
+                self.contactsByMobile[contact.mobile] = contact;
+            }
+            // TODO dont do this
+            self.currentContact = self.contacts[0];
+        }).then(function() {
+            // After initialization, setup our polling function
+            window.setInterval(function() {
+                Message.getNewMessages(self.identity).then(function(response) {
+                    if (response !== null && response.data.length !== 0) {
+                        self.processNewMessages(response.data);
+                    }
+                });
+            }, 2000);
+        });
     });
 });
